39e860065072605f36d959fcced1215a60ff616d
vicenteramirezp <vicente.ramirez@usm.cl>
2025-08-25 13:05:58 -0400

    Added crc32 module and its complemetary testbench

--- crc32_impl/crc32_impl.srcs/sources_1/new/crc32.sv
+++ crc32_impl/crc32_impl.srcs/sources_1/new/crc32.sv
@@ -22,14 +22,54 @@
 
 module crc32(
     input logic clk, rst, 
-    input logic crc_trigger,
+    input logic crc_trigger,data_done,
+    input logic [7:0] crc_32_in,
     output logic crc_busy, crc_finished,
-    output logic [31:0] crc_out32
+    output logic [31:0] crc_out32_xor
     );
 
-    localparam POLY = 33'h04C1_1DB7;
+    localparam POLY = 32'hEDB8_8320;
 
-    enum state {IDLE, BUSY, FINISHED} state, next_state;
+    typedef enum logic [1:0] {IDLE, Byte_Xor, Poly_Xor,Done} state_t;
+
+    logic [31:0] crc_out32;
+
+    assign crc_out32_xor=crc_out32^32'hFFFF_FFFF;
+
+
+    state_t state,next_state;
+
+    always_comb begin
+        crc_busy = 'b0;
+        crc_finished = 'b0;
+        next_state = state;
+        case (state)
+            IDLE: begin
+                if(crc_trigger)
+                    next_state = Byte_Xor;
+            end
+            Byte_Xor: begin
+                crc_busy = 'b1;
+                next_state=Poly_Xor;
+            end
+            Poly_Xor: begin
+                crc_busy = 'b1;
+                if(counter==3'h7)
+                    next_state=Done;
+
+            end
+            Done: begin
+                crc_finished = 'b1;
+                if(crc_trigger) begin
+                    next_state=Byte_Xor;
+                end
+                else if (data_done) begin
+                    next_state=IDLE;
+                end
+            end
+        endcase
+    end
+
 
     /* FSM that governs CRC functionality */
     always_ff @(posedge clk) begin
@@ -39,41 +79,50 @@ module crc32(
         state <= next_state;   
     end
 
-    always_comb begin
-        crc_busy = 'b0;
-        crc_finished = 'b0;
-        next_state = state;
+    logic [31:0]crc32_next;
 
+
+    always_comb begin 
+        crc32_next=crc_out32;
         case (state)
-            IDLE: begin
-                if(crc_trigger)
-                    next_state = BUSY;
-            end
-
-            BUSY: begin
-                crc_busy = 'b1;
-
-
-            end
-
-            FINISHED: begin
-                crc_finished = 'b1;
-
-            end
+        IDLE: begin
+            crc32_next=32'hFFFF_FFFF;
+        end
+        Byte_Xor: begin
+            crc32_next=crc_out32^crc_32_in;
+        end
+        Poly_Xor: begin
+            crc32_next=(crc_out32[0])?(crc_out32 >> 1) ^ POLY:(crc_out32 >> 1);
+        end
+        Done: begin
+        end
         endcase
-
     end
 
-    /* Registers that hold our current and future value */
 
-    always_ff @(posedge clk) begin
-        if (rst | crc_trigger)
-            crc_out32 = 32'hFFFF_FFFF;
+   
+    //Counter for Poly Xor
+    logic [2:0] counter,counter_next;
+
+    always_comb begin 
+        if(state==Poly_Xor)
+        counter_next=counter+1;
         else
-            crc_out32 = crc_out32;
+        counter_next=counter;
     end
 
 
+    always_ff @( posedge clk ) begin 
+    if (rst) begin
+        counter <= 0;
+        crc_out32<=32'hFFFF_FFFF;
+    end
+    else
+        begin
+        counter <= counter_next;   
+        crc_out32<=crc32_next;
+        end
+    end
 
 
 endmodule
